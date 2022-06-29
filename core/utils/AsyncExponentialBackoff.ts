9d3367675b90a5de3985a0de01b3988974727dc0
sirgallo <nickgallo97@gmail.com>
2022-06-28 21:52:32 -0400

    exp backoff

--- core/utils/AsyncExponentialBackoff.ts
+++ core/utils/AsyncExponentialBackoff.ts
@@ -4,24 +4,25 @@ import { LogProvider } from '@core/providers/LogProvider';
 const log = new LogProvider('Async Exponential Backoff');
 
 export async function asyncExponentialBackoff(
+  endpoint: string,
   retries: number, 
   timeout: number, 
   reqFunction: Function, 
   request: any,
-  depth = 1) {
+  depth = 1): Promise<any> {
   try {
     if (depth > retries) throw new Error(`Exceeded Max Retries: ${retries}`);
-    else return await reqFunction(request);
+    else return await reqFunction(endpoint, request);
   } catch (err) {
     if (depth > retries) throw err;
     else {
       const newTimeout = 2 * depth * timeout;
 
       log.info(`Moving to attempt: ${depth}`);
-      log.info(`Waiting for: ${newTimeout}`);
+      log.info(`Waiting for: ${newTimeout}ms`);
 
       await sleep(newTimeout);
-      return await asyncExponentialBackoff(retries, timeout, reqFunction, request, depth + 1);
+      return await asyncExponentialBackoff(endpoint, retries, timeout, reqFunction, request, depth + 1);
     }
   }
 }
